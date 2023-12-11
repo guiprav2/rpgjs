cd48eaeab77dfd04630951aa6c3f733e56851274
Gui Prá <gui@guiprav.com>
2023-12-11 03:34:45 -0300

    .

--- rpg.js
+++ rpg.js
@@ -19,12 +19,21 @@ rpg.viewport = function(children) {
   return div;
 };
 
-rpg.tilemap = function(tw, th, tmw, tmh, bg, tileset, children = []) {
+rpg.tilemap = function(tw, th, tmw, tmh, bg, tileset, roadblocks = [], children = []) {
   let div = document.createElement('div');
   div.className = 'tilemap';
   for (let [k, v] of Object.entries({ tw, th, tmw, tmh })) { div.style.setProperty(`--rpg-${k}`, v) }
   for (let [k, v] of Object.entries({ bg, tileset })) { div.style.setProperty(`--rpg-${k}`, `url("${v}")`) }
+  div.roadblocks = roadblocks;
   div.append(...children);
+
+  for (let tile of div.querySelectorAll('.tile')) {
+    let tx = Number(tile.style.getPropertyValue('--rpg-tx'));
+    let ty = Number(tile.style.getPropertyValue('--rpg-ty'));
+    let rb = roadblocks[ty]?.[tx] || 'OOOO';
+    if (rb[4] === 'H') { tile.style.setProperty('--rpg-high-tile', 2) }
+  }
+
   return div;
 };
 
@@ -40,6 +49,16 @@ rpg.tile = function(x, y, tx, ty, layer) {
   div.className = 'tile';
   for (let [k, v] of Object.entries({ x, y, tx, ty, layer })) { v != null && div.style.setProperty(`--rpg-${k}`, v) }
   return div;
+};
+
+rpg.findTiles = function(root, x, y) {
+  let tiles = [];
+  for (let tile of root.querySelectorAll('.tile')) {
+    let x2 = Number(tile.style.getPropertyValue('--rpg-x'));
+    let y2 = Number(tile.style.getPropertyValue('--rpg-y'));
+    if (x === x2 && y === y2) { tiles.push(tile) }
+  }
+  return tiles;
 };
 
 rpg.sprites = function(children) {
@@ -77,33 +96,75 @@ function heroFrame(sprite) {
 }
 
 heroFrame.transition = function(sprite) {
+  let end = false;
+
   switch (kbd.lastArrow) {
-    case 'ArrowDown':
-      sprite.style.setProperty('--rpg-sy', 0);
-      sprite.style.setProperty('--rpg-y', Number(sprite.style.getPropertyValue('--rpg-y')) + 1);
-      break;
-
-    case 'ArrowUp':
-      sprite.style.setProperty('--rpg-sy', 1);
-      sprite.style.setProperty('--rpg-y', Number(sprite.style.getPropertyValue('--rpg-y')) - 1);
-      break;
-
-    case 'ArrowRight':
-      sprite.style.setProperty('--rpg-sy', 2);
-      sprite.style.setProperty('--rpg-x', Number(sprite.style.getPropertyValue('--rpg-x')) + 1);
-      break;
-
-    case 'ArrowLeft':
-      sprite.style.setProperty('--rpg-sy', 3);
-      sprite.style.setProperty('--rpg-x', Number(sprite.style.getPropertyValue('--rpg-x')) - 1);
-      break;
-
-    default:
-      sprite.classList.remove('walking');
-      sprite.removeEventListener('transitionend', sprite.transitionHandler);
-      sprite.transitionHandler = null;
-      break;
+    case 'ArrowDown': end = !rpg.step(sprite, 0); break;
+    case 'ArrowUp': end = !rpg.step(sprite, 1); break;
+    case 'ArrowRight': end = !rpg.step(sprite, 2); break;
+    case 'ArrowLeft': end = !rpg.step(sprite, 3); break;
+    default: end = true; break;
   }
+
+  if (end) {
+    sprite.classList.remove('walking');
+    sprite.removeEventListener('transitionend', sprite.transitionHandler);
+    sprite.transitionHandler = null;
+  }
+};
+
+rpg.findSprite = function(root, x, y) {
+  for (let sprite of root.querySelectorAll('.sprite')) {
+    let x2 = Number(sprite.style.getPropertyValue('--rpg-x'));
+    let y2 = Number(sprite.style.getPropertyValue('--rpg-y'));
+    if (x === x2 && y === y2) { return sprite }
+  }
+  return null;
+};
+
+rpg.spriteUnblocked = function(sprite) {
+  let map = sprite.closest('.tilemap');
+  let sx = Number(sprite.style.getPropertyValue('--rpg-x'));
+  let sy = Number(sprite.style.getPropertyValue('--rpg-y'));
+  let dir = sprite.style.getPropertyValue('--rpg-sy') || '0';
+
+  for (let tile of rpg.findTiles(map, sx, sy)) {
+    let tx = Number(tile.style.getPropertyValue('--rpg-tx'));
+    let ty = Number(tile.style.getPropertyValue('--rpg-ty'));
+    let roadblocks = map.roadblocks[ty]?.[tx] || 'OOOO';
+    if (roadblocks[dir] === 'X') { return false }
+  }
+
+  let nsx = sx, nsy = sy, incomingDir;
+  switch (dir) {
+    case '0': nsy++; incomingDir = 1; break;
+    case '1': nsy--; incomingDir = 0; break;
+    case '2': nsx++; incomingDir = 3; break;
+    case '3': nsx--; incomingDir = 2; break;
+  }
+
+  for (let tile of rpg.findTiles(map, nsx, nsy)) {
+    let tx = Number(tile.style.getPropertyValue('--rpg-tx'));
+    let ty = Number(tile.style.getPropertyValue('--rpg-ty'));
+    let roadblocks = map.roadblocks[ty]?.[tx] || 'OOOO';
+    if (roadblocks[incomingDir] === 'X') { return false }
+  }
+
+  return !rpg.findSprite(map, nsx, nsy);
+};
+
+rpg.step = function(sprite, dir) {
+  sprite.style.setProperty('--rpg-sy', dir);
+  if (!rpg.spriteUnblocked(sprite)) { return false }
+
+  switch (Number(dir)) {
+    case 0: sprite.style.setProperty('--rpg-y', Number(sprite.style.getPropertyValue('--rpg-y')) + 1); break;
+    case 1: sprite.style.setProperty('--rpg-y', Number(sprite.style.getPropertyValue('--rpg-y')) - 1); break;
+    case 2: sprite.style.setProperty('--rpg-x', Number(sprite.style.getPropertyValue('--rpg-x')) + 1); break;
+    case 3: sprite.style.setProperty('--rpg-x', Number(sprite.style.getPropertyValue('--rpg-x')) - 1); break;
+  }
+
+  return true;
 };
 
 export default rpg;

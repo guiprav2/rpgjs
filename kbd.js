70a25f1b9eb7ebde4a2d8e5d76926cb1e2279074
Gui Prá <gui@guiprav.com>
2023-12-18 06:19:01 -0300

    .

--- kbd.js
+++ kbd.js
@@ -17,4 +17,86 @@ addEventListener('keyup', ev => {
   kbd.state[ev.key] = false;
 });
 
+const SWIPE_THRESHOLD = 50;
+const TAP_THRESHOLD = 5;
+const KEYPRESS_INTERVAL = 10;
+
+let touchStartX = 0;
+let touchStartY = 0;
+let touchEndX = 0;
+let touchEndY = 0;
+let touchActive = false;
+let keypressIntervalId;
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('touchend', handleTouchEnd, false);
+
+function handleTouchStart(event) {
+  touchActive = true;
+  touchStartX = touchEndX = event.touches[0].clientX;
+  touchStartY = touchEndY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  touchEndX = event.touches[0].clientX;
+  touchEndY = event.touches[0].clientY;
+
+  if (touchActive) {
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    if (Math.abs(deltaX) > SWIPE_THRESHOLD || Math.abs(deltaY) > SWIPE_THRESHOLD) {
+      !keypressIntervalId && handleSwipe(deltaX, deltaY);
+    }
+  }
+}
+
+function handleTouchEnd(event) {
+  touchActive = false;
+  clearInterval(keypressIntervalId);
+  keypressIntervalId = null;
+
+  const deltaX = touchEndX - touchStartX;
+  const deltaY = touchEndY - touchStartY;
+
+  if (Math.abs(deltaX) < TAP_THRESHOLD && Math.abs(deltaY) < TAP_THRESHOLD) {
+    simulateKeyPress('z');
+  }
+
+  touchStartX = touchStartY = touchEndX = touchEndY = 0;
+}
+
+function handleSwipe(deltaX, deltaY) {
+  clearInterval(keypressIntervalId);
+  keypressIntervalId = setInterval(() => {
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      // Horizontal swipe
+      if (deltaX > 0) {
+        simulateKeyPress('ArrowRight');
+      } else {
+        simulateKeyPress('ArrowLeft');
+      }
+    } else {
+      // Vertical swipe
+      if (deltaY > 0) {
+        simulateKeyPress('ArrowDown');
+      } else {
+        simulateKeyPress('ArrowUp');
+      }
+    }
+  }, KEYPRESS_INTERVAL);
+}
+
+function simulateKeyPress(key) {
+  const event = new Event('keydown');
+  event.key = key;
+  dispatchEvent(event);
+  requestAnimationFrame(() => {
+    const event = new Event('keyup');
+    event.key = key;
+    dispatchEvent(event);
+  });
+}
+
 export default kbd;
